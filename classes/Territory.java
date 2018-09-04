162c346b00e2156b170c6b90368e8d70cdb131c2
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2018-09-04 17:36:36 -0500

    partially implemented Territory.java
    and renamed Territories.java to Territory.java

---
+++ classes/Territory.java
@@ -0,0 +1,16 @@
+class Territory {
+	
+	private mapDataTypePlaceholder adjacentTerritories;
+	private int ID;
+	private int continent; // the continent ID that this territory belongs to
+		// or maybe use a function continentInstance.getParentContinent()?
+	
+	public int getID () { return ID; }
+	
+	public listTypePlaceholder getAdjacentTerritories() {
+		throw java.lang.UnsupportedOperationException;
+		return null;
+	}
+	
+	
+}
