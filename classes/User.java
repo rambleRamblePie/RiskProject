8c89ddb63ddd66a4a7fed130742d20eb385eb9de
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2018-09-06 04:35:58 -0500

    formatting, added constructor

--- classes/User.java
+++ classes/User.java
@@ -1,10 +1,18 @@
 class User {
-   private String username;
-   private int turn_position;
-   private int score;
-   private int numTroops;
-   public void Action(); // Move, Battle
-    //Not exactly sure how this function should look
-
-
+	private String username;
+	private int turn_position;
+	private int score;
+	private int numTroops;
+	
+	public void Action(); // Move, Battle
+	//Not exactly sure how this function should look
+	// Alex: Maybe use an enum?
+	
+	public User(String uname, int pos, int ntroops) {
+		username = uname;
+		turn_position = pos;
+		numTroops = ntroops;
+	}
+	
+	
 }
