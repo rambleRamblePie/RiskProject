0e4f1c644b05d3eb95ff053bbae2d05e8d531830
lrflores <luis.f1220@gmail.com>
2018-09-06 00:22:10 -0500

    added User class

---
+++ classes/User.java
@@ -0,0 +1,10 @@
+class User {
+   private String username;
+   private int turn_position;
+   private int score;
+   private int numTroops;
+   public void Action(); // Move, Battle
+    //Not exactly sure how this function should look
+
+
+}
