c2779cb7267ca8dea47b879a42688146ad82140e
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2018-09-06 05:19:54 -0500

    edited for clarity and naming conventions

--- classes/User.java
+++ classes/User.java
@@ -1,18 +1,25 @@
 class User {
 	private String username;
-	private int turn_position;
+	private int turnPosition;
 	private int score;
-	private int numTroops;
+	private int armyPower;
 	
 	public void Action(); // Move, Battle
 	//Not exactly sure how this function should look
 	// Alex: Maybe use an enum?
 	
-	public User(String uname, int pos, int ntroops) {
-		username = uname;
-		turn_position = pos;
-		numTroops = ntroops;
+	public User(String n, int startingArmy) {
+		username = n;
+		armyPower = startingArmy;
+		turnPosition = 0;
+		score = 0;
 	}
 	
+	public void setTurnPosition(int pos) {
+		turnPosition = pos;
+	}
 	
+	public int getTurnPosition() {
+		return turnPosition;
+	}
 }
