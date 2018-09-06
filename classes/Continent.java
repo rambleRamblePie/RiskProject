b33817917164ccc0843765ca413297a0fed378ef
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2018-09-06 04:32:53 -0500

    added constructors and some methods

--- classes/Continent.java
+++ classes/Continent.java
@@ -1,16 +1,31 @@
+import java.util.*;
+
 class Continent {
 	
 	private String name;
-	private int ID;
-	private listTypePlaceholder<Territory> territories;
+	// private int ID;
+	private ArrayList<Territory> territories;
 	private int bonusArmy;
 	
-	private bool isOccupiedByOneUser() {
-		throw java.lang.UnsupportedOperationException;
+	
+	Continent(String n, int b = 5) {
+		name = n;
+		bonusArmy = b;
+		// need to add territories, probably from a file
 	}
 	
-	// need accessor methods
+	public String getName() {
+		return name;
+	}
 	
+	public int getBonusArmyAmount() {
+		return bonusArmy;
+	}
 	
+	// should return the index of the user, otherwise returns -1
+	private int isOccupiedByOneUser(ArrayList<User> users) {
+		throw java.lang.UnsupportedOperationException();
+		// add armies to that user
+	}
 	
 }
--- classes/World.java
+++ classes/World.java
@@ -1,8 +1,25 @@
+import java.util.*;
+
 class World {
 	
-	private listTypePlaceholder<Continent> continents;
+	private ArrayList<Continent> continents;
 	
+	World() {
+		// maybe read from a file instead
+		continents.add(new Continent("North America", 5));
+		continents.add(new Continent("South America", 2));
+		continents.add(new Continent("Europe", 5));
+		continents.add(new Continent("Asia", 7));
+		continents.add(new Continent("Africa", 3));
+		continents.add(new Continent("Austrailia", 2));
+	}
 	
+	public getContinentByIndex(int i) {
+		//throw if not a valid index?
+		return continents.get(i);
+	}
 	
-	
+	public boolean checkForGameEnd() {
+		// game ends when only one player has armies on the board
+	}
 }
