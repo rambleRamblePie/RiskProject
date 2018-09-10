35af6c1cdc244e33e95915dd98000665b7e91c0a
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2018-09-10 04:36:43 -0500

    directory restructure for maven

---
+++ src/main/java/Risk.java
@@ -0,0 +1,13 @@
+package TeamHAL.risk;
+
+/**
+ * Hello world!
+ *
+ */
+public class Risk 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
