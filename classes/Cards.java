3a261cd76d3825cd72c394494bcd0e3d8b504542
AMitchell11 <42917225+AMitchell11@users.noreply.github.com>
2018-09-14 12:40:48 -0500

    Update Cards.java

--- classes/Cards.java
+++ classes/Cards.java
@@ -2,6 +2,7 @@ public class Card {
     
     //we may have to manually make each card and put it in an arraylist. no big deal
     //creates a card for a territory of a certain type
+    //will need a deck class that stores the deck, and possibly a hand class, and a hand variable for each player
     private Territory territory;
     private String type;
     
