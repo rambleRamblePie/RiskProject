5fb7cb5fe3291be77d7ad28cef12e660045afff5
lrflores <luis.f1220@gmail.com>
2018-09-18 16:18:42 -0500

    Added some comments to User class.

--- classes/User.java
+++ classes/User.java
@@ -12,8 +12,15 @@ class User {
 		switch (Actions){
 			case MOVE:
 				System.out.println(username + " is Moving __ units from __ to __");
+				//remove army power from one territory
+				//add to an adjacent territory
 			case ATTACK:
 				System.out.println(username + " is Attacking __ from __ with __ units");
+				//get number of attacking armies, and defending armies
+				// two sets of dice rolls depending on previous vars
+				//compare
+				// remove army power from each player and territory depending
+				//if there are no defending troops on attacked territory, change ownership
 			case PLACE_ARMY:
 				System.out.println(username + " is placing an army at__");
 
