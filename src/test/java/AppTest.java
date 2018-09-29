03ac5014b0024d20eb88a25e35017371eb0a2813
AMitchell11 <42917225+AMitchell11@users.noreply.github.com>
2018-09-28 21:19:51 -0500

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,38 +1,87 @@
-package com.TeamHAL.app;
+//package com.TeamHAL.Risk;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+//public class AppTest
+//extends TestCase
+//{
+/**
+ * Create the test case
+ *
+ * @param testName name of the test case
+ */
+//public AppTest( String testName ) {super( testName ); }
+
+/**
+ * @return the suite of tests being tested
+ */
+//public static Test suite(){return new TestSuite( AppTest.class ); }
+
+/**
+ * Rigourous Test :-)
+ */
+//public void testApp(){assertTrue( true );}
+
+//}
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    User user;
+    Dice dice;
+    Territory territory;
+    Territory territory2;
+    Continent continent;
+    Card card;
+    ArrayList<Territory> territories = new ArrayList<Territory>();
+
+    @Before
+    public void setup() throws Exception {
+        user = new User("Aaron", 25);
+        dice = new Dice();
+        territory = new Territory("Brazil");
+        territory2 = new Territory("Alaska");
+        card = new Card("Infantry", territory);
+        continent = new Continent("South America", 2, territories);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testPlayer() {
+        Assert.assertEquals(user.getUsername(), "Aaron");
+        Assert.assertEquals(user.getTurnPosition(), 0);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testContinent() {
+        Assert.assertEquals(continent.getName(), "South America");
+        Assert.assertEquals(continent.getBonusArmyAmount(), 2);
     }
+
+    @Test
+    public void testTerritory() {
+        Assert.assertEquals(territory.getName(), "Brazil");
+        //Assert.assertEquals(territory.getUser(), false);
+        Assert.assertEquals(territory.getArmyPower(), 0);
+        Assert.assertEquals(territory2.getName(), "Alaska");
+        Assert.assertEquals(territory2.getArmyPower(), 0);
+    }
+
+    @Test
+    public void testRoll() {
+        int result = dice.getFaceValue();
+        Assert.assertTrue(1 <= result && result <= 6);
+        Assert.assertTrue(result != 0);
+    }
+
+    @Test
+    public void testCard() {
+        Assert.assertEquals(card.getType(), "Infantry");
+    }
+
 }
