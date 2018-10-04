7cbee34763e77c56619aa1e783d2cdecde6fbb2a
Alexander Milligan <alexander.ryan.milligan@gmail.com>
2018-10-04 18:21:19 -0500

    i recommend to write to the file after each player's turn

--- src/main/java/GameRecorder.java
+++ src/main/java/GameRecorder.java
@@ -6,8 +6,11 @@ import java.text.*;
 // singleton class that is used to record all game actions
 // usage:
 // GameRecorder recorder = GameRecorder.INSTANCE;
-// recorder.record("player 1 attacks player 2 from <territory> to <territory>"
+// recorder.outputAndRecord("player 1 attacks player 2 from <territory> to <territory>"
 //     + " with <{1..3}> vs <{1..2}> armies");
+// String recordFileName = recorder.writeToFile();
+// ... after each turn, save to file:
+// recorder.writeToFile(recordFileName);
 public enum GameRecorder {
     INSTANCE;
 
@@ -21,19 +24,38 @@ public enum GameRecorder {
         lines.add(line);
     }
 
-    // returns a string of the filename.
-    public String writeToFile() throws IOException {
-        String fileName = new SimpleDateFormat("'game_'yyyyMMddHHmm'.txt'").format(new Date());
+    public void outputAndRecord(String line) {
+        lines.add(line);
+        System.out.println(line);
+    }
 
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName)))) {
-            
+    // writes all lines to a file
+    // returns a string of the filename
+    public String writeToFile() throws IOException {
+        String fileName = new SimpleDateFormat("'riskgame_'yyyyMMddHHmm'.txt'").format(new Date());
+
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName)))) {
             for (String line : lines) {
                 writer.write(line);
+                writer.newLine();
             }
             writer.close();
+            clearRecords();
         }
 
         return fileName;
+    }
+
+    // appends data to the end of the file
+    public void writeToFile(String fileName) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName, true)))) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.close();
+            clearRecords();
+        }
     }
 
     // useful for when a new game is played
