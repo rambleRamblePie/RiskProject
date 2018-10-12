016f72d59065fb2a707bdc076dbc956b65911d51
AMitchell11 <42917225+AMitchell11@users.noreply.github.com>
2018-10-12 17:13:24 -0500

    Added Javadoc Comments

--- src/main/java/Territory.java
+++ src/main/java/Territory.java
@@ -1,11 +1,22 @@
 import java.util.*;
 
+/**
+ * The Territory class contains information about the Territories used
+ * in the game including the army power, the User who has control of
+ * it and the adjacent territories. You can easily access the User's name,
+ * the Territories army count, and modify the army power with the provided
+ * functions
+ * @author Aaron Mitchell
+ * @author Alex Milligan
+ * @author Luis Florez
+ * @version 0.4
+ */
+
 public class Territory {
 
 	private String name;
 	private ArrayList<Territory> adjacentTerritories;
 	private Continent continent;
-	private boolean occupied;
 	private User userControlling;
 	private int armyPower;
 
@@ -15,17 +26,28 @@ public class Territory {
 		armyPower = 0;
 	}
 
-	// Used when spawning territories
+	/**
+	 * Adds adjacencies to the spawned territory
+	 * @see Territory
+	 */
 	public void addAdjacencies(ArrayList<Territory> adjacencies) {
 
 		this.adjacentTerritories = adjacencies;
 	}
 
-	// Checking if we can attack
+	/**
+	 * Grabs the adjacent territories
+	 * @see Territory
+	 */
 	public ArrayList<Territory> getAdjacentTerritories() {
 		return adjacentTerritories;
 	}
 
+	/**
+	 * Sets the controlling User of the Territory
+	 * @param user The User who will be set as the controller of the territory
+	 * @see Territory
+	 */
 	public void setOccupyingUser(User user) {
 		this.userControlling = user;
 	}
@@ -33,27 +55,55 @@ public class Territory {
 	// Need removeOccupyingUser?
 	// No. We can just recall setOccupyingUser() to set a new user without deleting original user
 
+	/**
+	 * Sets the number of armies for the Territory
+	 * @param armies The number of armies the Territory should have
+	 * @see Territory
+	 */
 	public void setNumArmies(int armies){
 		armyPower = armies;
 	}
 
+	/**
+	 * Returns the User object who is controlling the territory
+	 * @see Territory
+	 */
 	public User getUser() {
 		return userControlling;
 	}
 
+	/**
+	 * Sets the number of armies for the Territory
+	 * @param n The number of armies the Territory should have
+	 * @see Territory
+	 */
 	public void setArmyPower(int n) {
 		armyPower = n;
 	}
 
+	/**
+	 * Returns the currenty army power of the Territory
+	 * @return The current army power of the Territory
+	 * @see Territory
+	 */
 	public int getArmyPower() {
 		return armyPower;
 	}
 
-	// Adding explicity increment and decrement for undo function
+	/**
+	 * Increments the army power. Added to simplify the undo feature
+	 * @param addedArmies The number of armies to add to the territory
+	 * @see Territory
+	 */
 	public void incrementArmies(int addedArmies) {
 		armyPower = armyPower + addedArmies;
 	}
 
+	/**
+	 * Decrements the army power. Added to simplify the undo feature
+	 * @param lostArmies The number of armies the Territory will lose
+	 * @see Territory
+	 */
 	public void decrementArmies(int lostArmies) {
 
 		if(lostArmies > armyPower){
@@ -65,17 +115,29 @@ public class Territory {
 		}
 	}
 
-
-
-	//added for Card class
+	/**
+	 * Grabs the Territory's name
+	 * @return The name of the Territory
+	 * @see Territory
+	 */
 	public String getTerritory(){
 		return name;
 	}
 
+	/**
+	 * Grabs the continent the territory belongs to
+	 * @return The continent the territory belongs to
+	 * @see Territory
+	 */
 	public Continent getContinent() {
 		return continent;
 	}
 
+	/**
+	 * Grabs the name of the Territory
+	 * @return The name of the Territory
+	 * @see Territory
+	 */
 	public String getName() {
 		return name;
 	}
