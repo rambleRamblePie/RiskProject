222c2e33c4c8434f414b89bd81c4c404b278a734
AMitchell11 <42917225+AMitchell11@users.noreply.github.com>
2018-10-12 16:57:45 -0500

    Update Dice.java

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,34 +1,33 @@
 import java.lang.Math;
 
-class Dice {
-	Dice(){}
+/**
+ * A simple Dice class that spawns a die and rolls it
+ * @author Alex Miilligan
+ * @version 0.4
+ */
+
+public class Dice {
+
 	private int faceValue;
-	
+
+	Dice(){}
+
+	/**
+	 * Rolls the dice. The result will be a number between 1 and 6
+	 * @see Dice
+	 */
 	public void roll() {
 		faceValue = 1 + (int) Math.floor(Math.random() * 6);
 	}
-	
+
+	/**
+	 * Accesses the face value of the rolled die
+	 * @return Returns the value of the die rolled
+	 * @see Dice
+	 */
 	public int getFaceValue() {
 		roll();
 		return faceValue;
 	}
 	
-	
-	
 }
-
-
-// javac Dice.java
-// java *.class
-/*
-class test {
-	public static void main (String args[]) {
-		Dice d = new Dice();
-		
-		for (int i = 0; i < 30; i++) {
-			d.roll();
-			System.out.println("value: " + d.getFaceValue());
-		}
-	}
-}
-*/
