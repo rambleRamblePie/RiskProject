35f122e21bcc127052ac3ce9f3492a26cbc9b597
AMitchell11 <42917225+AMitchell11@users.noreply.github.com>
2018-10-12 21:46:01 -0500

    Fixed Maven Errors

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,11 +3,24 @@ import java.util.Scanner;
 import java.util.HashMap;
 import java.util.ArrayList;
 
+/** The User class contains the information used to setup User objects and
+ *  call functions to ensure functionality of the game. These include adding
+ *  territories, grabbing the username, setting army power, incrementing User
+ *  credits etc.
+ * @author Aaron Mitchell
+ * @author Alex Milligan
+ * @author Luis Florez
+ * @version 0.4
+ */
+
 public class User {
 	private String username;
 	private int turnPosition;
 	private int score;
 	private int armyPower;
+	private int twitterCount;
+	private int credits;
+	private boolean hasCredits;
 	private HashMap<String,Territory> territoriesHeld;
 	private HashMap<String,Continent> continentsHeld;
 	/*
@@ -94,18 +107,39 @@ public class User {
 		this.username = name;
 		this.armyPower = startingArmy;
 		score = 0;
+		this.twitterCount = 0;
+		this.credits = 0;
 
 		territoriesHeld = new HashMap<String,Territory>();
 		continentsHeld = new HashMap<String,Continent>();
 	}
 
+	/**
+	 * Sets the User's turn position
+	 * @param position Specifies the User's turn position
+	 * @see User
+	 */
 	public void setTurnPosition(int position) {
 		turnPosition = position;
 	}
+
+
+	/**
+	 * Returns the turn position
+	 * @return User's turn position
+	 * @see User
+	 */
 	public int getTurnPosition() {
 		return turnPosition;
 	}
 
+	/**
+	 * Adds to the user's army power
+	 * @param p The power to add to the User's army power
+	 * @return User's updated army power
+	 * @throws IllegalArgumentException if number entered is negative
+	 * @see User
+	 */
 	public int addArmyPower(int p) {
 		if (p < 0)
 			throw new java.lang.IllegalArgumentException();
@@ -114,6 +148,38 @@ public class User {
 		return armyPower;
 	}
 
+
+	/**
+	 * Keeps track of User's total conquered territories over the course of the game
+	 * @param increase The amount to add to User's total conquered territories
+	 * @return User's updated conquered territory total
+	 * @see User
+	 * @see TweetPoster
+	 */
+	public int incrementTwitterCount(int increase){
+	    twitterCount = twitterCount + increase;
+
+	    return twitterCount;
+    }
+
+
+	/**
+	 * Returns User's current count of conquered territories in the game so far
+	 * @return User's current conquered territory amount
+	 * @see User
+	 * @see TweetPoster
+	 */
+    public int getTwitterCount(){
+		return twitterCount;
+	}
+
+	/**
+	 * Removes army power if User has been attacked and lost the duel
+	 * @param p The power to remove from the User's army power
+	 * @return User's updated total number of armies
+	 * @throws IllegalArgumentException if the number entered is negative
+	 * @see User
+	 */
 	public int removeArmyPower(int p) {
 		if (p < 0)
 			throw new java.lang.IllegalArgumentException();
@@ -127,50 +193,181 @@ public class User {
 		return armyPower;
 	}
 
+
+	/**
+	 * Keeps track of User's total win amount
+	 * @param newScore The value to update score to
+	 * @see User
+	 */
 	public void setScore(int newScore){
 		score = newScore;
 	}
 
+	/**
+	 * Grabs User's total win amount
+	 * @return User's current win amount
+	 * @see User
+	 */
 	public int getScore(){
 		return score;
 	}
 
+
+	/**
+	 * Grabs User's username they chose at the beginning of the game
+	 * @return User's name
+	 * @see User
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Adds specified territory to User's HashMap. Each user will have a HashMap to keep track
+	 * @param territory The territory to add to User's HashMap
+	 * of their conquered territories
+	 * @see User
+	 * @see Territory
+	 */
 	public void addTerritory(Territory territory) {
 
 		System.out.println(username + " now occupies " + territory.getName() + "!");
 		territoriesHeld.put(territory.getName(), territory);
 	}
 
+	/**
+	 * Deletes specified territory from User's HashMap. Each user will have a HashMap to keep track
+	 * of their conquered territories
+	 * @param territoryName The territory name to remove from User's HashMap
+	 * @see User
+	 * @see Territory
+	 */
 	public void deleteTerritory(String territoryName)
 	{
 		System.out.println(username + " has lost control of " + territoryName);
 		territoriesHeld.remove(territoryName);
 	}
 
-	/*
-	May need to adjust HashMap for this function
-
-	public ArrayList<String> getUserCountries()
-	{
-		return ArrayList<String>(territoriesHeld) // Trying to get keys, may need to change to objects
+	/**
+	 * Increments User's current amount of credits. Each User will gain 1 credit per turn.
+	 * Credit can be used to buy cards, purchase an undo feature, buy armies or transfer
+	 * their credits to another player
+	 * @return User's credits
+	 * @see User
+	 */
+	public int incrementCredit(){
+		credits = credits + 1;
+		return credits;
 	}
 
-	*/
 
+	/**
+	 * Used to figure out how many armies to give a User at the beginning of their turn
+	 * @return Number of armies to give to User
+	 * @see User
+	 */
+	public int numArmiesAdded(){
+		int add = territoriesHeld.size() / 3;
+
+		if(add < 3){
+			add = 3;
+		}
+
+		return add;
+	}
+
+	/**
+	 * Decrements User's current amount of credits. Each User will gain 1 credit per turn.
+	 * Credit can be used to buy cards, purchase an undo feature, buy armies or transfer
+	 * their credits to another player
+	 * @param creditAmount The amount of credit to remove from User's credits
+	 * @return User's credits
+	 * @see User
+	 */
+	public int removeCredit(int creditAmount){
+		if((credits - creditAmount) < 0)
+		{
+			credits = 0;
+		}
+		else {
+			credits = credits - creditAmount;
+		}
+		return credits;
+	}
+
+	/**
+	 * Used to check whether a User is allowed to purchase things with their credits
+	 * @param numRequiredCredits The amount of credits required for the User to buy what they are requesting
+	 * @return True if they have enough credits, False if they do not
+	 * @see User
+	 */
+	public boolean enoughCredits(int numRequiredCredits){
+		int remainingCredits = credits - numRequiredCredits;
+
+		if(remainingCredits < 0){
+			hasCredits = false;
+		}
+		else{
+			hasCredits = true;
+			credits = remainingCredits;
+		}
+		return hasCredits;
+	}
+
+	/**
+	 * Transfers credits between players
+	 * @param user1 The User who is transfering the credits
+	 * @param user2 The User who is receiving the credits
+	 * @see User
+	 */
+	public void transferCredits(User user1, User user2){
+		// will display names of all users in userList in loop
+		// grab input of name from player transferring credit (input being string of the name)
+		// add user1's credits to user2
+		// set user1's credits to 0
+	}
+
+	/**
+	 * Returns all the territories a User has control of
+	 * @return All territories occupied by User
+	 * @see User
+	 * @see Territory
+	 */
+	public ArrayList<Territory> getUserTerritories()
+	{
+		return new ArrayList<Territory>(territoriesHeld.values());
+	}
+
+
+	/**
+	 * Returns User's total army power
+	 * @return User's total number of armies
+	 * @see User
+	 */
 	public int getArmyPower(){
 		return armyPower;
 	}
 
+	/**
+	 * Add continent to User's continent HashMap. Each user has a continent HashMap
+	 * that is used to store their controlled continents
+	 * @param continent The continent to add to User's HashMap
+	 * @see User
+	 * @see Continent
+	 */
 	public void addContinent(Continent continent)
 	{
 		System.out.println(username + " is dominating the continent of " + continent);
 		continentsHeld.put(continent.getName(), continent);
 	}
 
+	/**
+	 * Delete continent from User's continent HashMap. Each user has a continent HashMap
+	 * that is used to store their controlled continents
+	 * @param continentName The continent name to query to add to User's HashMap
+	 * @see User
+	 * @see Territory
+	 */
 	public void deleteContinent(String continentName)
 	{
 		System.out.println(username + " is no longer dominating the continent of " + continentName);
