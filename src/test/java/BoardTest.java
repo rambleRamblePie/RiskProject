04a6fd5bdeb4d6e7549be864c846cb733c0b2254
AMitchell11 <42917225+AMitchell11@users.noreply.github.com>
2018-11-19 15:28:59 -0600

    Update BoardTest.java

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,10 +1,8 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Arrays;
 
 
