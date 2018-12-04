66eaf5e15e667670a67de696fa160989c6d30e0b
rambleRamblePie <rambleramblepie@users.noreply.github.com>
2018-12-04 11:36:49 -0600

    Refactored card_deck constructor by using a while loop. Also created a string array to hold the types of cards.
    Contributed by Henry Rodriguez, Luis Flores

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -28,56 +29,71 @@ public class Deck {
         // Initialize card_deck
         card_deck = new ArrayList<Card>();
 
-        // Spawn and add the 42 cards to the card_deck
-        card_deck.add(new Card("Cannon", territories.get(0)));
-        card_deck.add(new Card("Horse", territories.get(1)));
-        card_deck.add(new Card("Tank", territories.get(2)));
-        card_deck.add(new Card("Cannon", territories.get(3)));
-        card_deck.add(new Card("Horse", territories.get(4)));
-        card_deck.add(new Card("Tank", territories.get(5)));
-        card_deck.add(new Card("Cannon", territories.get(6)));
-        card_deck.add(new Card("Horse", territories.get(7)));
-        card_deck.add(new Card("Tank", territories.get(8)));
-        card_deck.add(new Card("Cannon", territories.get(9)));
-        card_deck.add(new Card("Horse", territories.get(10)));
-        card_deck.add(new Card("Tank", territories.get(11)));
-        card_deck.add(new Card("Cannon", territories.get(12)));
-        card_deck.add(new Card("Horse", territories.get(13)));
-        card_deck.add(new Card("Tank", territories.get(14)));
-        card_deck.add(new Card("Cannon", territories.get(15)));
-        card_deck.add(new Card("Horse", territories.get(16)));
-        card_deck.add(new Card("Tank", territories.get(17)));
-        card_deck.add(new Card("Cannon", territories.get(18)));
-        card_deck.add(new Card("Horse", territories.get(19)));
-        card_deck.add(new Card("Tank", territories.get(20)));
-        card_deck.add(new Card("Cannon", territories.get(21)));
-        card_deck.add(new Card("Horse", territories.get(22)));
-        card_deck.add(new Card("Tank", territories.get(23)));
-        card_deck.add(new Card("Cannon", territories.get(24)));
-        card_deck.add(new Card("Horse", territories.get(25)));
-        card_deck.add(new Card("Tank", territories.get(26)));
-        card_deck.add(new Card("Cannon", territories.get(27)));
-        card_deck.add(new Card("Horse", territories.get(28)));
-        card_deck.add(new Card("Tank", territories.get(29)));
-        card_deck.add(new Card("Cannon", territories.get(30)));
-        card_deck.add(new Card("Horse", territories.get(31)));
-        card_deck.add(new Card("Tank", territories.get(32)));
-        card_deck.add(new Card("Cannon", territories.get(33)));
-        card_deck.add(new Card("Horse", territories.get(34)));
-        card_deck.add(new Card("Tank", territories.get(35)));
-        card_deck.add(new Card("Cannon", territories.get(36)));
-        card_deck.add(new Card("Horse", territories.get(37)));
-        card_deck.add(new Card("Tank", territories.get(38)));
-        card_deck.add(new Card("Cannon", territories.get(39)));
-        card_deck.add(new Card("Horse", territories.get(40)));
-        card_deck.add(new Card("Tank", territories.get(41)));
+        // Type of cards in deck
+        String[] cardDeck = {"Cannon", "Horse", "Tank"};
+
+        // Spawn and add the 42 cards to the card_deck using while loop.
+        int i = 0;
+        int j = 0;
+        while (i <= 41) {
+            if (j == 3) {
+                j = 0;
+                card_deck.add(new Card(cardDeck[j], territories.get(i)));
+            }
+            i++;
+            j++;
+            System.out.println("i :" + i + "j: " + j);
+        }
+
+//        card_deck.add(new Card("Cannon", territories.get(0)));
+//        card_deck.add(new Card("Horse", territories.get(1)));
+//        card_deck.add(new Card("Tank", territories.get(2)));
+//        card_deck.add(new Card("Cannon", territories.get(3)));
+//        card_deck.add(new Card("Horse", territories.get(4)));
+//        card_deck.add(new Card("Tank", territories.get(5)));
+//        card_deck.add(new Card("Cannon", territories.get(6)));
+//        card_deck.add(new Card("Horse", territories.get(7)));
+//        card_deck.add(new Card("Tank", territories.get(8)));
+//        card_deck.add(new Card("Cannon", territories.get(9)));
+//        card_deck.add(new Card("Horse", territories.get(10)));
+//        card_deck.add(new Card("Tank", territories.get(11)));
+//        card_deck.add(new Card("Cannon", territories.get(12)));
+//        card_deck.add(new Card("Horse", territories.get(13)));
+//        card_deck.add(new Card("Tank", territories.get(14)));
+//        card_deck.add(new Card("Cannon", territories.get(15)));
+//        card_deck.add(new Card("Horse", territories.get(16)));
+//        card_deck.add(new Card("Tank", territories.get(17)));
+//        card_deck.add(new Card("Cannon", territories.get(18)));
+//        card_deck.add(new Card("Horse", territories.get(19)));
+//        card_deck.add(new Card("Tank", territories.get(20)));
+//        card_deck.add(new Card("Cannon", territories.get(21)));
+//        card_deck.add(new Card("Horse", territories.get(22)));
+//        card_deck.add(new Card("Tank", territories.get(23)));
+//        card_deck.add(new Card("Cannon", territories.get(24)));
+//        card_deck.add(new Card("Horse", territories.get(25)));
+//        card_deck.add(new Card("Tank", territories.get(26)));
+//        card_deck.add(new Card("Cannon", territories.get(27)));
+//        card_deck.add(new Card("Horse", territories.get(28)));
+//        card_deck.add(new Card("Tank", territories.get(29)));
+//        card_deck.add(new Card("Cannon", territories.get(30)));
+//        card_deck.add(new Card("Horse", territories.get(31)));
+//        card_deck.add(new Card("Tank", territories.get(32)));
+//        card_deck.add(new Card("Cannon", territories.get(33)));
+//        card_deck.add(new Card("Horse", territories.get(34)));
+//        card_deck.add(new Card("Tank", territories.get(35)));
+//        card_deck.add(new Card("Cannon", territories.get(36)));
+//        card_deck.add(new Card("Horse", territories.get(37)));
+//        card_deck.add(new Card("Tank", territories.get(38)));
+//        card_deck.add(new Card("Cannon", territories.get(39)));
+//        card_deck.add(new Card("Horse", territories.get(40)));
+//        card_deck.add(new Card("Tank", territories.get(41)));
 
 
         /* Testing
         System.out.println(card_deck.get(0).getType());
         System.out.println(card_deck.get(0).getName());
         */
-        
+
         // Shuffle the card_deck
         Collections.shuffle(card_deck);
     }
@@ -112,5 +128,5 @@ public class Deck {
     public void addCard(Card card){
         card_deck.add(card);
     }
-    
+
 }
